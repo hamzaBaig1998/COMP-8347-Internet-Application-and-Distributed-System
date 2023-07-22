6d810d1150add421923693fa00cdd8b6a0aac629
Hamza Baig <39631430+hamzaBaig1998@users.noreply.github.com>
2023-07-22 12:43:01 -0400

    views refactored

--- Project/comp8347/blog/views.py
+++ Project/comp8347/blog/views.py
@@ -1,23 +1,73 @@
-from django.shortcuts import render,get_object_or_404,redirect
-from blog.models import Post
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from blog.forms import PostForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import (TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView )
-
-# Create your views here.
-
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from blog.models import Post
+from blog.forms import PostForm
 
 class PostListView(ListView):
-    context_object_name = 'posts'
-    template_name = 'blog/post_list.html'
+    # specify the model and template name
     model = Post
+    template_name = 'blog/post_list.html'
 
+    # specify the context object name for the list of posts
+    context_object_name = 'posts'
+
+    # override the queryset to filter posts by created date and order by descending created date
     def get_queryset(self):
         return Post.objects.filter(created_date__lte=timezone.now()).order_by('-created_date')
 
 class PostDetailView(DetailView):
-    context_object_name = 'post'
-    template_name = 'blog/post_detail.html'
+    # specify the model and template name
     model = Post
+    template_name = 'blog/post_detail.html'
+
+    # specify the context object name for the post
+    context_object_name = 'post'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    # specify the model, form class, and template name
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    # override the success URL to redirect to the newly created post's detail page
+    success_url = reverse_lazy('blog:post_list')
+
+    # set the author of the post to the current user
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    # specify the model, form class, and template name
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    # override the success URL to redirect to the updated post's detail page
+    def get_success_url(self):
+        return reverse_lazy('blog:post_detail', kwargs={'pk': self.object.pk})
+
+    # restrict updating to the author of the post
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            return redirect('blog:post_list')
+        return super().dispatch(request, *args, **kwargs)
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    # specify the model and template name
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+
+    # override the success URL to redirect to the post list
+    success_url = reverse_lazy('blog:post_list')
+
+    # restrict deleting to the author of the post
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            return redirect('blog:post_list')
+        return super().dispatch(request, *args, **kwargs)
--- Project/comp8347/memberships/views.py
+++ Project/comp8347/memberships/views.py
@@ -1,172 +1,176 @@
-from django.shortcuts import render,redirect
-from django.views.generic import TemplateView,ListView,DetailView,View
-from memberships.models import Membership,UserMembership,Subscription
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-import stripe
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from memberships.models import Membership, UserMembership, Subscription
 
-# Create your views here.
+import stripe
+
+User = get_user_model()
+
+# helper function to get the user's membership
+@login_required
 def get_user_membership(request):
     user_membership_qs = UserMembership.objects.filter(user=request.user)
-    if user_membership_qs.exists():
-        return user_membership_qs.first()
-    return None
+    return user_membership_qs.first() if user_membership_qs.exists() else None
 
+# helper function to get the user's subscription
+@login_required
 def get_user_subscription(request):
-    user_subscription_qs = Subscription.objects.filter(user_membership = get_user_membership(request))
-    if user_subscription_qs.exists():
-        user_subscription = user_subscription_qs.first()
-        return user_subscription
-    return None
+    user_membership = get_user_membership(request)
+    user_subscription_qs = Subscription.objects.filter(user_membership=user_membership)
+    return user_subscription_qs.first() if user_subscription_qs.exists() else None
 
+# helper function to get the selected membership
+@login_required
 def get_selected_membership(request):
-    membership_type =  request.session['selected_membership_type']
+    membership_type = request.session.get('selected_membership_type')
     selected_membership_qs = Membership.objects.filter(membership_type=membership_type)
-    if selected_membership_qs.exists():
-        return selected_membership_qs.first()
-    return None
+    return selected_membership_qs.first() if selected_membership_qs.exists() else None
 
 class MembershipSelectView(ListView):
     template_name = 'memberships/membership_list.html'
     context_object_name = 'memberships'
     model = Membership
 
+    # add the current membership to the context
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         current_membership = get_user_membership(self.request)
-        context['current_membership'] = str(current_membership.membership)
+        context['current_membership'] = str(current_membership.membership) if current_membership else None
         return context
 
-    def post(self,request,*args,**kwargs):
+    # handle the form submission to select a membership
+    def post(self, request, *args, **kwargs):
         selected_membership_type = request.POST.get('membership_type')
 
-        user_membership = get_user_membership(self.request)
-        user_subscription = get_user_subscription(self.request)
+        # get the user's current membership and subscription
+        user_membership = get_user_membership(request)
+        user_subscription = get_user_subscription(request)
 
-        selected_membership_qs = Membership.objects.filter(membership_type=selected_membership_type)
-        if selected_membership_qs.exists():
-            selected_membership = selected_membership_qs.first()
+        # get the selected membership
+        selected_membership = get_selected_membership(request)
+        if not selected_membership:
+            messages.error(request, 'Invalid membership type selected')
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
-        """
-        Validation to check if user memberships == selected_membership
-        """
-        if user_membership.membership == selected_membership:
-            if user_subscription != None:
-                messages.info(request, 'The selected membership is your current Memberships, your next payment would be due by {}'.format('get this value from stripe'))
+        # check if the user is already subscribed to the selected membership
+        if user_membership and user_membership.membership == selected_membership:
+            if user_subscription and user_subscription.active:
+                messages.info(request, 'You are already subscribed to this membership plan.')
                 return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-        # PASS THE SELECTED_MEMBERSHIP INTO THE SESSION TO BE ABLE TO PAS IT INTO THE NEXT VIEWW
-        request.session['selected_membership_type'] = selected_membership.membership_type
-        # the above code is passing the value of enterprise or professional into the next veiw
-        return HttpResponseRedirect(reverse('memberships:payment'))
 
+        # store the selected membership type in the session and redirect to the payment page
+        request.session['selected_membership_type'] = selected_membership.membership_type
+        return HttpResponseRedirect(reverse('memberships:payment'))
 
 @login_required
 def PaymentView(request):
+    # get the user's current membership, selected membership, and subscription
     user_membership = get_user_membership(request)
-    try:
-        selected_membership = get_selected_membership(request)
-    except:
-        return redirect(reverse("memberships:select_membership"))
+    selected_membership = get_selected_membership(request)
+
+    if not selected_membership:
+        messages.error(request, 'Invalid membership type selected')
+        return redirect(reverse('memberships:select_membership'))
+
+    # set up Stripe
     publishKey = settings.STRIPE_PUBLISHABLE_KEY
     if request.method == "POST":
-        # try:
+        # process the payment
         token = request.POST['stripeToken']
 
-        # UPDATE FOR STRIPE API CHANGE 2018-05-21
+        try:
+            customer = stripe.Customer.retrieve(user_membership.stripe_customer_id)
+            customer.source = token
+            customer.save()
 
-        '''
-        First we need to add the source for the customer
-        '''
+            subscription = stripe.Subscription.create(
+                customer=user_membership.stripe_customer_id,
+                items=[
+                    {"plan": selected_membership.stripe_plan_id},
+                ]
+            )
 
-        customer = stripe.Customer.retrieve(user_membership.stripe_customer_id)
-        customer.source = token # 4242424242424242
-        customer.save()
+            # redirect to the transaction update page
+            return redirect(reverse('memberships:update_transaction',
+                                     kwargs={
+                                         'subscription_id': subscription.id
+                                     }))
+        except stripe.error.StripeError as e:
+            # handle any Stripe errors
+            messages.error(request, f"An error has occurred: {e}")
 
-        '''
-        Now we can create the subscription using only the customer as we don't need to pass their
-        credit card source anymore
-        '''
-
-        subscription = stripe.Subscription.create(
-            customer=user_membership.stripe_customer_id,
-            items=[
-                { "plan": selected_membership.stripe_plan_id },
-            ]
-        )
-        print(subscription.id)
-
-        return redirect(reverse('memberships:update_transaction',
-                                kwargs={
-                                    'subscription_id': subscription.id
-                                }))
-
-        # except:
-        #     messages.info(request, "An error has occurred, investigate it in the console")
-
+    # render the payment page
     context = {
         'publishKey': publishKey,
         'selected_membership': selected_membership
     }
-
     return render(request, "memberships/membership_payment.html", context)
 
 @login_required
 def UpdateTransactionRecords(request, subscription_id):
+    # get the user's current membership and selected membership
     user_membership = get_user_membership(request)
     selected_membership = get_selected_membership(request)
+
+    if not selected_membership:
+        messages.error(request, 'Invalid membership type selected')
+        return redirect(reverse('memberships:select_membership'))
+
+    # update the user's membership and subscription records
     user_membership.membership = selected_membership
     user_membership.save()
 
-    sub, created = Subscription.objects.get_or_create(
-        user_membership=user_membership)
+    sub, created = Subscription.objects.get_or_create(user_membership=user_membership)
     sub.stripe_subscription_id = subscription_id
     sub.active = True
     sub.save()
 
+    # remove the selected membership type from the session
     try:
         del request.session['selected_membership_type']
-    except:
-        pass
-
-    messages.info(request, 'Successfully created {} membership'.format(
-        selected_membership))
+    except KeyError:
+        pass    # display a success message and redirect to the membership selection page
+    messages.success(request, f'Successfully created {selected_membership} membership')
     return redirect(reverse('memberships:select_membership'))
 
 @login_required
 def CancelSubscription(request):
-    user_sub = get_user_subscription(request)
+    # get the user's current membership and subscription
+    user_membership = get_user_membership(request)
+    user_subscription = get_user_subscription(request)
 
-    if user_sub.active is False:
-        messages.info(request, "You dont have an active membership")
+    if not user_membership or not user_subscription or not user_subscription.active:
+        messages.error(request, "You don't have an active membership")
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
-    sub = stripe.Subscription.retrieve(user_sub.stripe_subscription_id)
+    # cancel the subscription in Stripe and update the user's membership and subscription records
+    sub = stripe.Subscription.retrieve(user_subscription.stripe_subscription_id)
     sub.delete()
 
-    user_sub.active = False
-    user_sub.save()
+    user_subscription.active = False
+    user_subscription.save()
 
     free_membership = Membership.objects.get(membership_type='Free')
-    user_membership = get_user_membership(request)
     user_membership.membership = free_membership
     user_membership.save()
-    user = get_object_or_404(User,username=request.username)
-    user_email = user.email
 
-    messages.info(
-        request, "Successfully cancelled membership. We have sent an email")
-    # sending an email here
+    # send an email to the user to confirm the cancellation
+    user_email = user_membership.user.email
+    messages.success(request, "Successfully cancelled membership. An email has beensent to you.")
     send_mail(
         'Subscription successfully cancelled',
-        'Successfully cancelled membership. We have sent an email',
+        'Successfully cancelled membership. An email has been sent to you.',
         'adebisiayomide68@gmail.com',
         [user_email],
         fail_silently=False,
     )
-    
-    return redirect(reverse('memberships:select'))
+
+    # redirect to the membership selection page
+    return redirect(reverse('memberships:select_membership'))
