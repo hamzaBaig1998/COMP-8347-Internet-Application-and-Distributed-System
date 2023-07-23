012401fa57f621908416d5f3eb4abd82925f9a36
Vivek <verma17@uwindsor.ca>
2023-07-23 17:17:13 -0400

    linking courses to club

--- Project/comp8347/courses/models.py
+++ Project/comp8347/courses/models.py
@@ -2,12 +2,16 @@ from django.db import models
 # from memberships.models import Membership
 from django.contrib.auth.models import User
 from django.urls import reverse
+from club.models import Club
+
+
 # Create your models here.
 
 class Request(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     phone = models.CharField(max_length=20)
+
 
 class Category(models.Model):
     category = models.CharField(max_length=150)
@@ -19,6 +23,7 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'categories'
 
+
 class Course(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     slug = models.SlugField()
@@ -29,7 +34,7 @@ class Course(models.Model):
     duration = models.CharField(max_length=10, help_text='Please use the following formats: 1 Week or 1 Month')
     starting_date = models.DateField(null=True)
     ending_date = models.DateField(null=True)
-    # allowed_memberships = models.ManyToManyField(Membership, related_name='memberships_allowed')
+    club = models.ManyToManyField(Club)
 
     def __str__(self):
         return self.title
@@ -37,12 +42,11 @@ class Course(models.Model):
     def get_absolute_url(self):
         return reverse("courses:course_detail", kwargs={"slug": self.slug})
 
-    # def get_courses_related_to_memberships(self):
-    #     return self.courses.all()
-
     @property
     def lessons(self):
         return self.lesson_set.all().order_by('position')
+
+
 class Lesson(models.Model):
     slug = models.SlugField()
     title = models.CharField(max_length=30)
