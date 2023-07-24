a9a06fc4fa062fa0bfdff94cd512fa3162652700
Vivek <verma17@uwindsor.ca>
2023-07-24 09:50:12 -0400

    Adding the club detail to the profile which is a relation on the club

--- Project/comp8347/club/views.py
+++ Project/comp8347/club/views.py
@@ -10,31 +10,8 @@ from club.FxUtil import compute_fx_amount, SYMBOL_MAP, clear_fx_cache
 from club.forms import TierForm, FxForm, PaymentForm
 from club.models import Club, Fx, Order
 from courses.models import Course
+from users.models import Profile, User
 
-
-# Create your views here.
-# def get_user_membership(request):
-#     user_membership_qs = UserMembership.objects.filter(user=request.user)
-#     if user_membership_qs.exists():
-#         return user_membership_qs.first()
-#     return None
-#
-#
-# def get_user_subscription(request):
-#     user_subscription_qs = Subscription.objects.filter(user_membership=get_user_membership(request))
-#     if user_subscription_qs.exists():
-#         user_subscription = user_subscription_qs.first()
-#         return user_subscription
-#     return None
-#
-#
-# def get_selected_membership(request):
-#     membership_type = request.session['selected_membership_type']
-#     selected_membership_qs = Membership.objects.filter(membership_type=membership_type)
-#
-#     if selected_membership_qs.exists(): return selected_membership_qs.first()
-#     return None
-#
 
 def compute_fx_details(price):
     data = {}
@@ -103,6 +80,11 @@ def save_order(session_data, card_details, outcome):
                                  card_last_6=card_detail,
                                  result=outcome)
     print(f"Saved order {order}")
+    club = Club.objects.filter(tier=tier).first()
+    profile = Profile.objects.filter(user=User.objects.get(id=user_id)).first()
+    profile.set_club(club)
+    profile.save()
+    print(f"Profile of {profile} updated with club being {profile.club}")
     return order
 
 
@@ -154,142 +136,3 @@ class PayView(View):
                 print("Payment failed!")
                 save_order(request.session, request.POST, False)
                 return render(request, "club/paymentFail.html")
-
-# class MembershipSelectView(ListView):
-#     template_name = 'club/club_details.html'
-#     context_object_name = 'club'
-#     model = Membership
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         current_membership = get_user_membership(self.request)
-#         context['current_membership'] = str(current_membership.membership)
-#         return context
-#
-#     def post(self, request, *args, **kwargs):
-#         selected_membership_type = request.POST.get('membership_type')
-#
-#         user_membership = get_user_membership(self.request)
-#         user_subscription = get_user_subscription(self.request)
-#
-#         selected_membership_qs = Membership.objects.filter(membership_type=selected_membership_type)
-#         if selected_membership_qs.exists():
-#             selected_membership = selected_membership_qs.first()
-#
-#         """
-#         Validation to check if user club == selected_membership
-#         """
-#         if user_membership.membership == selected_membership:
-#             if user_subscription != None:
-#                 messages.info(request, 'The selected club is your current Memberships, your next payment would be due by {}'.format('get this value from stripe'))
-#                 return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-#         # PASS THE SELECTED_MEMBERSHIP INTO THE SESSION TO BE ABLE TO PAS IT INTO THE NEXT VIEWW
-#         request.session['selected_membership_type'] = selected_membership.membership_type
-#         # the above code is passing the value of enterprise or professional into the next veiw
-#         return HttpResponseRedirect(reverse('club:payment'))
-#
-#
-# @login_required
-# def PaymentView(request):
-#     user_membership = get_user_membership(request)
-#     try:
-#         selected_membership = get_selected_membership(request)
-#     except:
-#         return redirect(reverse("club:select_membership"))
-#     publishKey = settings.STRIPE_PUBLISHABLE_KEY
-#     if request.method == "POST":
-#         # try:
-#         token = request.POST['stripeToken']
-#
-#         # UPDATE FOR STRIPE API CHANGE 2018-05-21
-#
-#         '''
-#         First we need to add the source for the customer
-#         '''
-#
-#         customer = stripe.Customer.retrieve(user_membership.stripe_customer_id)
-#         customer.source = token  # 4242424242424242
-#         customer.save()
-#
-#         '''
-#         Now we can create the subscription using only the customer as we don't need to pass their
-#         credit card source anymore
-#         '''
-#
-#         subscription = stripe.Subscription.create(
-#             customer=user_membership.stripe_customer_id,
-#             items=[
-#                 {"plan": selected_membership.stripe_plan_id},
-#             ]
-#         )
-#         print(subscription.id)
-#
-#         return redirect(reverse('club:update_transaction',
-#                                 kwargs={
-#                                     'subscription_id': subscription.id
-#                                 }))
-#
-#         # except:
-#         #     messages.info(request, "An error has occurred, investigate it in the console")
-#
-#     context = {
-#         'publishKey': publishKey,
-#         'selected_membership': selected_membership
-#     }
-#
-#     return render(request, "club/payment.html", context)
-#
-#
-# @login_required
-# def UpdateTransactionRecords(request, subscription_id):
-#     user_membership = get_user_membership(request)
-#     selected_membership = get_selected_membership(request)
-#     user_membership.membership = selected_membership
-#     user_membership.save()
-#
-#     sub, created = Subscription.objects.get_or_create(user_membership=user_membership)
-#     sub.stripe_subscription_id = subscription_id
-#     sub.active = True
-#     sub.save()
-#
-#     try:
-#         del request.session['selected_membership_type']
-#     except:
-#         pass
-#
-#     messages.info(request, 'Successfully created {} club'.format(selected_membership))
-#     return redirect(reverse('club:select_membership'))
-#
-#
-# @login_required
-# def CancelSubscription(request):
-#     user_sub = get_user_subscription(request)
-#
-#     if user_sub.active is False:
-#         messages.info(request, "You dont have an active club")
-#         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-#
-#     sub = stripe.Subscription.retrieve(user_sub.stripe_subscription_id)
-#     sub.delete()
-#
-#     user_sub.active = False
-#     user_sub.save()
-#
-#     free_membership = Membership.objects.get(membership_type='Free')
-#     user_membership = get_user_membership(request)
-#     user_membership.membership = free_membership
-#     user_membership.save()
-#     user = get_object_or_404(User, username=request.username)
-#     user_email = user.email
-#
-#     messages.info(request, "Successfully cancelled club. We have sent an email")
-#     # sending an email here
-#     send_mail(
-#         'Subscription successfully cancelled',
-#         'Successfully cancelled club. We have sent an email',
-#         'adebisiayomide68@gmail.com',
-#         [user_email],
-#         fail_silently=False,
-#     )
-#
-#     return redirect(reverse('club:select'))
--- Project/comp8347/users/models.py
+++ Project/comp8347/users/models.py
@@ -1,22 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+
+from club.models import Club
+
+
 # Create your models here.
 
 class Profile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=200)
-    profile_pic = models.ImageField(default='default.jpg', upload_to = 'profile_pics')
+    profile_pic = models.ImageField(default='default.jpg', upload_to='profile_pics')
     is_teacher = models.BooleanField(default=False)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE, default='1')
 
     def __str__(self):
         return self.user.username + ' Profile'
+
+    def set_club(self, club):
+        self.club = club
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
         img = Image.open(self.profile_pic.path)
-        if img.height >100 or img.width>100:
-            output_size = (200,200)
+        if img.height > 100 or img.width > 100:
+            output_size = (200, 200)
             img.thumbnail(output_size)
             img.save(self.profile_pic.path)
